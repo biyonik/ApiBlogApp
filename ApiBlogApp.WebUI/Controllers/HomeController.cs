872bd48e57bff778cb0a82490ca7c29cf63acabb
Ahmet Altun <ahmet.altun60@hotmail.com>
2021-02-15 20:31:39 +0300

    Web UI katmanı oluşturuldu

---
+++ ApiBlogApp.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiBlogApp.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
